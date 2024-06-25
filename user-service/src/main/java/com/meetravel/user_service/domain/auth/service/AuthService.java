f97e92eb1d519c622c63c00af61d179e57d6ffe2
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-25 20:13:20 +0900

    fix: 임시토큰 로직 추가 및 기회원 여부 타입 및 변수명 변경

--- user-service/src/main/java/com/meetravel/user_service/domain/auth/service/AuthService.java
+++ user-service/src/main/java/com/meetravel/user_service/domain/auth/service/AuthService.java
@@ -1,16 +1,11 @@
 package com.meetravel.user_service.domain.auth.service;
 
 
-import com.meetravel.user_service.domain.auth.dto.request.SignUpRequest;
 import com.meetravel.user_service.domain.auth.dto.response.KakaoToken;
 import com.meetravel.user_service.domain.auth.dto.response.LoginResponse;
 import com.meetravel.user_service.domain.auth.properties.KakaoTokenRequestProperties;
-import com.meetravel.user_service.domain.user.entity.UserEntity;
-import com.meetravel.user_service.domain.user.enums.Role;
 import com.meetravel.user_service.domain.user.enums.SocialType;
 import com.meetravel.user_service.domain.user.repository.UserRepository;
-import com.meetravel.user_service.global.exception.BadRequestException;
-import com.meetravel.user_service.global.exception.ErrorCode;
 import com.meetravel.user_service.global.jwt.JwtService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +25,7 @@ public class AuthService {
 
     /**
      * 카카오 로그인/JWT 발급
+     *
      * @param authorizationCode
      * @param response
      * @return
@@ -43,19 +39,24 @@ public class AuthService {
         // 2. ID 토큰 정보 받아오기
         KakaoToken.IdToken idToken = this.getKakaoIdToken(kakaoToken.getIdToken());
 
-        // 3. 받아온 ID 토큰으로부터 카카오 회원 고유번호 추출, Dto 회원 여부 N 설정
+        // 3. 받아온 ID 토큰으로부터 카카오 회원 고유번호 추출, Dto 회원 여부 true, 임시토큰 여부 false 설정(default)
         String userId = idToken.getSub() + SocialType.KAKAO.getSocialSuffix();
-        String userYn = "N";
-        
+        boolean isRegisteredUser = true;
+        boolean isTemporaryToken = false;
+
+
+        /**
+         * 기존 회원이 아닌 경우, 회원가입 대상 -> isUser 구분 값 false 설정 및 임시토큰 발급 대상 설정
+         */
+        if (!userRepository.findByUserId(userId).isPresent()) {
+            isRegisteredUser = false;
+            isTemporaryToken = true;
+        }
+
         // 4. JWT 생성 및 헤더에 싣기
-        LoginResponse loginResponse = jwtService.createJwtToken(userId);
+        LoginResponse loginResponse = jwtService.createJwtToken(userId, isTemporaryToken);
         jwtService.setHeaderAccessToken(response, loginResponse.getAccessToken());
         jwtService.setHeaderRefreshToken(response, loginResponse.getRefreshToken());
-
-        // 회원이면 회원여부를 Y로 설정
-        if (userRepository.findByUserId(userId).isPresent()) {
-            userYn = "Y";
-        }
 
         // 로그인 응답
         return LoginResponse.builder()
@@ -66,17 +67,18 @@ public class AuthService {
                 .refreshToken(loginResponse.getRefreshToken())
                 .refreshTokenExpiresIn(loginResponse.getRefreshTokenExpiresIn())
                 .socialType(SocialType.KAKAO)
-                .userYn(userYn)
+                .isRegisteredUser(isRegisteredUser)
                 .build();
     }
 
     /**
      * 카카오 토큰 받아오기
+     *
      * @param authorizationCode
      * @return
      */
     private KakaoToken getKakaoToken(String authorizationCode) {
-        MultiValueMap<String,String> kakaoTokenRequestBody = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> kakaoTokenRequestBody = new LinkedMultiValueMap<>();
         kakaoTokenRequestBody.add("grant_type", kakaoTokenRequestProperties.getGrantType());
         kakaoTokenRequestBody.add("client_id", kakaoTokenRequestProperties.getClientId());
         kakaoTokenRequestBody.add("redirect_uri", kakaoTokenRequestProperties.getRedirectUri());
@@ -88,43 +90,12 @@ public class AuthService {
 
     /**
      * 카카오 ID토큰 정보 받아오기
+     *
      * @param idToken
      * @return
      */
     private KakaoToken.IdToken getKakaoIdToken(String idToken) {
         return kakaoAuthFeignClient.getIdTokenInfo(idToken);
-    }
-
-    /**
-     * 회원가입
-     * @param signUpRequest
-     */
-    @Transactional
-    public void signUp(SignUpRequest signUpRequest) {
-        
-        // 이미 존재하는 회원 ID인지 검증
-        if (userRepository.findByUserId(signUpRequest.getUserId()).isPresent()) {
-            throw new BadRequestException(ErrorCode.ALREADY_EXISTS_MEMBER_ID);
-        }
-
-        UserEntity userEntity = UserEntity.builder()
-                .userId(signUpRequest.getUserId())
-                .name(signUpRequest.getName())
-                .nickName(signUpRequest.getNickName())
-                .phoneNumber(signUpRequest.getPhoneNumber())
-                .profileImage(signUpRequest.getProfileImage())
-                .travelCount(signUpRequest.getTravelCount())
-                .travelStyle1(signUpRequest.getTravelStyle1())
-                .travelStyle2(signUpRequest.getTravelStyle2())
-                .mbti(signUpRequest.getMbti())
-                .hobby(signUpRequest.getHobby())
-                .intro(signUpRequest.getIntro())
-                .socialType(signUpRequest.getSocialType())
-                .role(Role.USER)
-                .build();
-
-        userRepository.save(userEntity);
-
     }
 
 
