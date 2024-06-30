6aa1dd9a693e0ee8373b4c5b484ebad0a914bc10
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-30 21:04:56 +0900

    refactor: 회원가입 api 분리로 인한 기존 코드 삭제 -> 추후에 회원 관련 서비스 구현 예정

--- user-service/src/main/java/com/meetravel/user_service/domain/user/controller/UserControllerDoc.java
+++ user-service/src/main/java/com/meetravel/user_service/domain/user/controller/UserControllerDoc.java
@@ -1,16 +1,10 @@
 package com.meetravel.user_service.domain.user.controller;
 
 
-import com.meetravel.user_service.domain.auth.dto.request.SignUpRequest;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "사용자(유저) API")
 public interface UserControllerDoc {
 
-    @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
-    void signUp(@RequestBody @Valid SignUpRequest signUpRequest);
 
 }
