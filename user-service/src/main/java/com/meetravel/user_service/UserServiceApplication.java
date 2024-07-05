4d99b83c66e59475f4f8ab0beca121def6a48775
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-05 21:36:34 +0900

    feat: module-common @ComponentScan 추가

--- user-service/src/main/java/com/meetravel/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/meetravel/user_service/UserServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages={"com.meetravel.module_common"})
 @SpringBootApplication
 public class UserServiceApplication {
 
