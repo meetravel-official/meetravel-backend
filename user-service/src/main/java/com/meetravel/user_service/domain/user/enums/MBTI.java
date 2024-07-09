2c34950805ec9a2c58139d97bca2decc9fd70983
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-10 05:16:27 +0900

    refactor: MBTI Enum

---
+++ user-service/src/main/java/com/meetravel/user_service/domain/user/enums/MBTI.java
@@ -0,0 +1,14 @@
+package com.meetravel.user_service.domain.user.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MBTI {
+    ISTJ, ISFJ, INFJ, INTJ,
+    ISTP, ISFP, INFP, INTP,
+    ESTP, ESFP, ENFP, ENTP,
+    ESTJ, ESFJ, ENFJ, ENTJ;
+
+}
