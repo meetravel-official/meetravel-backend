0c797c9606c8d81d76dedc868f8369f1dcbeb55d
Karatus <shm1193@gmail.com>
2024-07-24 18:10:55 +0900

    feat: matching-service init

---
+++ matching-service/src/test/java/com/meetravel/matchingservice/MatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetravel.matchingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
