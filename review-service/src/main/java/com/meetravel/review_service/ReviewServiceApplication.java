f6b2f81d83d5de977c961ba85632dc633a1035ed
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-24 21:09:18 +0900

    feat: review-service

---
+++ review-service/src/main/java/com/meetravel/review_service/ReviewServiceApplication.java
@@ -0,0 +1,15 @@
+package com.meetravel.review_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages={"com.meetravel.module_common","com.meetravel.review_service"})
+@SpringBootApplication
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+}
