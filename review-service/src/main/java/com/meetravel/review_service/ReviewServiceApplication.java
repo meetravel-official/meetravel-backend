b834fe35f7904fbaee6003c5c97f40d568c4fb16
jaesungyoun <jaesungyoun@lotte.net>
2024-07-26 10:43:05 +0900

    fix: 리뷰 반영

--- review-service/src/main/java/com/meetravel/review_service/ReviewServiceApplication.java
+++ review-service/src/main/java/com/meetravel/review_service/ReviewServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @ComponentScan(basePackages={"com.meetravel.module_common","com.meetravel.review_service"})
 @SpringBootApplication
 public class ReviewServiceApplication {
