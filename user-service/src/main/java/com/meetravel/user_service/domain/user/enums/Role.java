9ffdb1ea1e90798a7a72305b8b4d3c1758ad06c7
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-09 19:59:02 +0900

    refactor: 생성자 직접 생성

--- user-service/src/main/java/com/meetravel/user_service/domain/user/enums/Role.java
+++ user-service/src/main/java/com/meetravel/user_service/domain/user/enums/Role.java
@@ -4,11 +4,16 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum Role {
     USER("ROLE_USER","사용자"), ADMIN("ROLE_ADMIN","관리자");
 
     private final String value;
     private final String desc;
+
+    Role(String value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
 
 }
